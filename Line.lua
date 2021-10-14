9157a0e10d8ff1846d3bb1ba8a0aecf4639b3e86
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2021-10-14 10:57:48 +0530

    init

---
+++ Line.lua
@@ -0,0 +1,31 @@
+local function draw(hyp, origin, thickness, parent, color, l) 
+	local line = l or Instance.new("Frame")
+	line.Name = "Constraint"
+	line.AnchorPoint = Vector2.new(.5, .5)
+	line.Size = UDim2.new(0, hyp, 0, thickness or 1)
+	line.BackgroundColor3 = color or Color3.new(1, 1, 1)
+	line.BorderSizePixel = 0
+	line.Position = UDim2.fromOffset(origin.x, origin.y)
+	line.ZIndex = 5
+	line.Parent = parent
+
+	return line
+end
+
+return function (origin, endpoint, parent, thickness, color, l)
+	local adj = (Vector2.new(endpoint.x, origin.y) - Vector2.new(origin.x, origin.y)).Magnitude
+	local opp = (Vector2.new(endpoint.x, origin.y) - Vector2.new(endpoint.x, endpoint.y)).Magnitude
+	local hyp = math.sqrt(adj^2 + opp^2)
+
+	local line = l and draw(hyp, origin, thickness, parent, color, l) or draw(hyp, origin, thickness, parent, color)		
+	local mid = Vector2.new((origin.x + endpoint.x)/2, (origin.y + endpoint.y)/2)
+
+	local theta = math.atan2(origin.y - endpoint.y, origin.x - endpoint.x)
+	theta /= math.pi
+	theta *= 180
+
+	line.Position = UDim2.fromOffset(mid.x, mid.y)
+	line.Rotation = theta
+
+	return line
+end
